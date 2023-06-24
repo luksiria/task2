abd233b83e49cd4504def09a710b7bfd3665f709
Kris <kristina_lacrima@mail.ru>
2023-06-24 14:45:48 +0300

    Ready

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int price = 13675;
+        int amount = 20;
+        boolean isRegistered = true;
+
+        System.out.println(price / amount);
+
+    }
+}
+
